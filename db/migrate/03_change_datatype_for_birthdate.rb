dc21ca1608e79466750f795ad78905df1e9ccb91
Yingqi Chen <chanwk18@hotmail.com>
2019-06-02 07:25:38 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,7 +1,9 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
 
   def change
-    change_table :students, :birthdate, :datetime
+    change_table :students do |t|
+      t.datetime :birthdate
+    end
   end
 
 
